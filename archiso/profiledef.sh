88596de6eec7b4696f48c9ffc6245212d95dd326
nexus-r21 <nexusx@duck.com>
2025-07-06 16:56:49 +0530

    hardened malloc and allow users in the group wheel to run commands as root.

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -17,7 +17,6 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/doas.conf"]="0:0:400"
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
