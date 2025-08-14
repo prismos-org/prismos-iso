80309322969f4ed78ce02502bac5955ebde80276
nexus-r21 <nexus-x@tuta.io>
2025-08-14 19:35:53 +0530

    Update sysctl values and Increase the faillock lockout to 24 hours.
    Signed-off-by: nexus-r21 <nexus-x@tuta.io>

--- archiso/airootfs/etc/profile.d/hardened_malloc.sh
+++ archiso/airootfs/etc/profile.d/hardened_malloc.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/sh
 # Copyright 2025 The Secureblue Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,5 +11,4 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#!/usr/bin/sh
 export LD_PRELOAD=/usr/lib/libhardened_malloc.so
